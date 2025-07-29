76ce106c88c26b4985d989cc42e03a51580544c0
Leonardo <lmestremadoyro@gmail.com>
2025-07-29 03:14:23 -0500

    vercel analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,7 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import { ThemeProvider } from "@/components/theme-provider";
 import { SpeedInsights } from "@vercel/speed-insights/next";
+import { Analytics } from "@vercel/analytics/next";
 
 const inter = Inter({ subsets: ["latin"] });
 
